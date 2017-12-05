0c8aea6f3e00aa781ab91a3518f90fc6a455e345
Mr. Plant <lucas92ag@gmail.com>
2017-12-05 20:51:38 -0300

    Se crearon las funciones tableroDePuntuación() y ganadorDelTorneo()

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,4 +1,59 @@
-def ligaDeFutbol(lista):
+def tableroDePuntuacion(lista):
+
+    tablero = {}
+
+    for tupla in lista:
+
+        if tupla[1] > tupla[3]:
+
+            if tupla[0] in tablero:
+                tablero[tupla[0]] += 2
+            else:
+                tablero[tupla[0]] = 2
+
+        if tupla[1] < tupla[3]:
+
+            if tupla[2] in tablero:
+                tablero[tupla[2]] += 2
+            else:
+                tablero[tupla[2]] = 2
+
+        if tupla[1] == tupla[3]:
+
+            if tupla[0] in tablero:
+                tablero[tupla[0]] += 1
+            else:
+                tablero[tupla[0]] = 1
+
+            if tupla[2] in tablero:
+                tablero[tupla[2]] += 1
+
+            else:
+                tablero[tupla[2]] = 1
+
+    return tablero
+
+def ganadorDelTorneo(dict):
+
+    for key in dict:
+        if dict[key] ==
+
+    maximum = max(dict, key=dict.get)
+
+    return maximum
+
+
+
+
+
+
+
+print(ganadorDelTorneo(tableroDePuntuacion([("Boca", 0, "Belgrano", 0), ("Boca", 0, "Almagro", 1), ("Almagro", 1, "Belgrano", 2)])))
+
+print(tableroDePuntuacion([("Boca", 0, "Belgrano", 1), ("Boca", 0, "Almagro", 1), ("Almagro", 1, "Belgrano", 2)]))
+
+print(tableroDePuntuacion([("a", 1, "b", 0), ("a", 1, "b", 0), ("c", 1, "a", 2)]))
+
 
 
 
