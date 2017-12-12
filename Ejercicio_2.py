ddee822a0e2c84cbc222526063b4760a83a9a63a
Lucas92ag <Lucas92ag@gmail.com>
2017-12-12 20:09:00 -0300

    Se crean los primeras cuatro pruebas unitarias del Ejercicio_2.

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -111,9 +111,9 @@ def ejercicio2(var1,var2):
 
 posicionesDeDisparosDePrueba = [(1,1),(3,4),(1,3),(4,5)]
 
-assert (ejercicio2([],posicionesDeDisparosDePrueba) == [])
-assert (ejercicio2([""],posicionesDeDisparosDePrueba) == [])
-assert (ejercicio2(["      "],posicionesDeDisparosDePrueba) == [])
+#assert (ejercicio2([],posicionesDeDisparosDePrueba) == [])
+#assert (ejercicio2([""],posicionesDeDisparosDePrueba) == [])
+#assert (ejercicio2(["      "],posicionesDeDisparosDePrueba) == [])
 assert (ejercicio2(["soy NO valido"],posicionesDeDisparosDePrueba) == [])
 assert (ejercicio2(["yo","tambien","soy","invalido"],posicionesDeDisparosDePrueba) == [])
 assert (ejercicio2(["b.b.","....","..bb","b.b"],posicionesDeDisparosDePrueba) == [])
